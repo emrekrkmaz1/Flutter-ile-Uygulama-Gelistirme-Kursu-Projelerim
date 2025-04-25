6598f72546422d64cefb1315d4ea1740a40fed30
Emre Korkmaz <111706457+emrekrkmaz1@users.noreply.github.com>
2025-04-25 16:37:07 +0300

    BÖLÜM-1 Yapılan alıştırmalar
    bölüm 1 de şu konular işlendi :
    Değişken Oluşturma
    Print() Metodu Kullanımı
    Değişken Oluşturma Çeşitleri
    Değişken Kapsamı
    Constant - Sabitler
    Kaçış Karakterleri ve Yorum Satırları
    Aritmetik Operatörler
    Tür Dönüşümü
    Konsol Girdisi
    Android Studio Hata Analizi - Dart
    NOT: main dosyası otomatik gelen bir dosya

---
+++ DegiskenOlusturmaCesitleri.dart
@@ -0,0 +1,50 @@
+void main (){
+  //örnek1
+  var sayi = 10;
+  int numara = 88;
+
+  //örnek 2
+  var fiyat = 12.99;
+  double ucret = 23.59;
+  print(fiyat);
+  print(ucret);
+
+  fiyat = 67.99;
+  ucret = 10.45;
+  print(fiyat);
+  print(ucret);
+
+  //örnek3
+  var sayi1 = 10 ;
+  var sayi2 = 20 ;
+  var toplam = sayi1 + 50;
+  print(toplam);
+  int carpma = sayi1 * sayi2;
+  print(carpma);
+
+//örnek4
+var sonuc1;
+int sonuc2;
+
+sonuc1 = 333;
+sonuc2 = 555;
+print(sonuc1);
+print(sonuc2);
+
+//örnek 5
+var s1,s2,s3;
+s1 =10;
+s2=20;
+s3=30;
+print("$s1=s1   $s2=s2   $s3=s3");
+
+int k1 = 44, k2 = 78;
+print(k1);
+print(k2);
+
+//örnek6 type safety yapının kendisini otomatk uyarması (tam sayı ise tam sayı ile devam etmeli)
+var deger =100;
+deger = 40;
+
+
+}
