bef490e56034d0241530e7587cfe3bbbfb09e2c4
Emre Korkmaz <111706457+emrekrkmaz1@users.noreply.github.com>
2025-05-07 20:45:48 +0300

    Add files via upload

---
+++ AmasyaElması.dart
@@ -0,0 +1,12 @@
+import 'package:nesne_tabanli_programlama_2/Elma.dart';
+
+class AmasyaElmasi extends Elma{
+  @override
+  void howToEat() {
+    print("Yıka ve Ye");
+  }
+  void howToSqueeze(){
+    print("Blanderdan geçir ve iç");
+  }
+
+}
